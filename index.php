8866450c2caedec424d8e3d944bc4befa64d5a2e
imanolcier21 <92285440+imanolcier21@users.noreply.github.com>
2024-11-19 14:47:33 -0600

    Primer commit
    Se agregan los archivos necesarios para el sistema

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include_once 'templates/head.php'; ?>
+<title>MUNDO EMPLEO | CENTRO AMERICA</title>
+<?php include_once 'templates/style.php'; ?>
+<?php include_once 'templates/header.php'; ?>
+<?php include_once 'templates/leftmunu.php'; ?>
+<center>
+<img src="img/slider/escritorio/1.png" alt="" style="width 100%; height:100% ">
+</center>
+<?php include_once 'templates/footer.php'; ?>
+<?php include_once 'templates/script.php'; ?>
